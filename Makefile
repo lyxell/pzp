fead9fd0f1e8853cf749920401be4a673ecee4d1
lyxell <alyxell@kth.se>
2021-01-10 10:10:26 +0100

    Clean up

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 pzp: main.c
-	$(CC) -O2 -static main.c -lncursesw -ltinfo -o $@
+	$(CC) -Wall -O2 -static main.c -lncursesw -ltinfo -o $@
 
 install: pzp
 	install pzp /usr/local/bin/pzp
--- main.c
+++ main.c
@@ -2,33 +2,48 @@
 #include <string.h>
 #include <locale.h>
 
-#define TERMINAL_HEIGHT LINES
+#define TERMINAL_HEIGHT ((size_t)LINES)
 #define KEY_ESCAPE 27
 #define KEY_RETURN 10
 
-void render(int num_options, char** items, int y,
-            int scroll_from_top, char* header) {
-    int i = 0;
-    if (header) {
-        mvaddstr(0, 2, header);
-        i += 2;
+struct state {
+    size_t selected_option;
+    int scroll_offset;
+    char* header;
+    char** options;
+    int num_options;
+    char* output;
+    char* output_on_key_left;
+    int is_exiting;
+};
+
+void render_state(struct state s) {
+
+    // print header and save the number of lines that was written
+    size_t lines_written = 0;
+    if (s.header) {
+        mvaddstr(0, 2, s.header);
+        lines_written += 2;
     }
-    for (int j = i; j < LINES; j++) {
-        if (j - i + scroll_from_top < num_options) {
-            if (j - i + scroll_from_top == y) {
-                mvaddstr(j, 0, "> ");
+
+    // print the options, only print as many as will fit the screen
+    for (int i = 0; i < TERMINAL_HEIGHT - lines_written; i++) {
+        if (i + s.scroll_offset < s.num_options) {
+            if (i + s.scroll_offset == s.selected_option) {
+                mvaddstr(i + lines_written, 0, "> ");
             } else {
-                mvaddstr(j, 0, "  ");
+                mvaddstr(i + lines_written, 0, "  ");
             }
-            addstr(items[j - i + scroll_from_top]);
+            addstr(s.options[i + s.scroll_offset]);
         } else {
-            move(j, 0);
+            move(i + lines_written, 0);
         }
         clrtoeol();
     }
+
 }
 
-void setup() {
+void setup_terminal() {
     setlocale(LC_ALL, "");
     newterm(NULL, stderr, stdin);
     noecho();
@@ -36,98 +51,98 @@ void setup() {
     keypad(stdscr, true);
     set_escdelay(100);
     use_default_colors();
-    // hide cursor
     curs_set(0);
 }
 
-void update_scroll_from_top(int y, int* scroll_from_top) {
-    if (y < *scroll_from_top) {
-        *scroll_from_top = y;
-    } else if (y > *scroll_from_top + TERMINAL_HEIGHT - 1) {
-        *scroll_from_top = y - TERMINAL_HEIGHT + 1;
+void teardown_terminal() {
+    refresh();
+    endwin();
+}
+
+struct state handle_input(struct state s, int input) {
+    switch (input) {
+    case 'g':
+        s.selected_option = 0;
+        break;
+    case 'G':
+        s.selected_option = s.num_options - 1;
+        break;
+    case 'h':
+        if (!s.output_on_key_left) break;
+        s.output = s.output_on_key_left;
+        s.is_exiting = 1;
+        break;
+    case 'k':
+        if (s.selected_option == 0) break;
+        s.selected_option -= 1;
+        break;
+    case 'j':
+        if (s.selected_option == s.num_options - 1) break;
+        s.selected_option += 1;
+        break;
+    case 'q':
+        s.is_exiting = 1;
+        break;
+    case 'l':
+    case KEY_RETURN:
+        s.output = s.options[s.selected_option];
+        s.is_exiting = 1;
+        break;
+    default:
+        break;
     }
+    return s;
+}
+
+struct state update_scroll_offset(struct state s) {
+    if (s.selected_option < s.scroll_offset) {
+        s.scroll_offset = s.selected_option;
+    } else if (s.selected_option > s.scroll_offset + TERMINAL_HEIGHT - 1) {
+        s.scroll_offset = s.selected_option - TERMINAL_HEIGHT + 1;
+    }
+    return s;
 }
 
 int main(int argc, char* argv[]) {
 
-    setup();
+    setup_terminal();
 
-    int y = 0;
-    int scroll_from_top = 0;
+    struct state s = {0};
+    s.options = argv + 1;
+    s.num_options = argc - 1;
 
-    char** on_left = NULL;
-    char* header = NULL;
-
-    char** args = argv;
-    int nargs = argc;
-
-    for (int i = 1; i < nargs; i++) {
-        if (strcmp("--on-left", args[i]) == 0) {
-            on_left = args + i + 1;
-            argc -= 2;
-            argv += 2;
-        } else if (strcmp("--header", args[i]) == 0) {
-            header = args[i+1];
-            argc -= 2;
-            argv += 2;
+    // parse command line options
+    for (int i = 1; i < argc; i++) {
+        if (strcmp("--on-left", argv[i]) == 0) {
+            s.output_on_key_left = argv[i + 1];
+            s.options += 2;
+            s.num_options -= 2;
+        } else if (strcmp("--header", argv[i]) == 0) {
+            s.header = argv[i+1];
+            s.options += 2;
+            s.num_options -= 2;
         }
     }
-
-    int num_options = argc - 1;
-    char** options = argv + 1;
-
-    char* output = NULL;
-
-    while (true)
-    {
-        update_scroll_from_top(y, &scroll_from_top);
-        render(num_options, options, y, scroll_from_top, header);
+    
+    // render loop
+    while (true) {
+        s = update_scroll_offset(s);
+        render_state(s);
         int input = getch();
-        int exit = 0;
-        switch (input) {
-        case 'g':
-            y = 0;
-            break;
-        case 'G':
-            y = num_options - 1;
-            break;
-        case 'h':
-            if (on_left) {
-                output = *on_left;
-                exit = 1;
-            }
-            break;
-        case 'k':
-            if (y > 0) {
-                y -= 1;
-            }
-            break;
-        case 'j':
-            if (y < num_options - 1) {
-                y += 1;
-            }
-            break;
-        case 'q':
-            exit = 1;
-            break;
-        case 'l':
-        case KEY_RETURN:
-            output = options[y];
-            exit = 1;
-            break;
-        default:
-            break;
-        }
-        if (exit) {
+        s = handle_input(s, input);
+        if (s.is_exiting) {
             break;
         }
     }
-    // refresh display
-    refresh();
-    // end ncurses mode
-    endwin();
-    if (output) {
-        puts(output);
+
+    teardown_terminal();
+
+    // print output if any
+    if (s.output) {
+        puts(s.output);
     }
+
     return 0;
+
 }
+
